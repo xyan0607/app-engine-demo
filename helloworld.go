69515cb4510cd7152d1f8dfbf018625a6defc782
xyan0607 <xyan0607@gmail.com>
2020-07-16 13:32:57 -0700

    Create helloworld.go

---
+++ helloworld.go
@@ -0,0 +1,42 @@
+package main
+
+// [START import]
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+// [END import]
+// [START main_func]
+
+func main() {
+	http.HandleFunc("/", indexHandler)
+
+	// [START setting_port]
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+		log.Printf("Defaulting to port %s", port)
+	}
+
+	log.Printf("Listening on port %s", port)
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		log.Fatal(err)
+	}
+	// [END setting_port]
+}
+
+// [END main_func]
+
+// [START indexHandler]
+
+// indexHandler responds to requests with our greeting.
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+	fmt.Fprint(w, "Hello, World!")
+}
